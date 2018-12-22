e9f23aa1ccdbcba423221abb5cd604aa203392c8
Peter Naydenov <free@backformation.com>
2018-12-22 05:59:31 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,82 @@
+const Fsm = require ( '@peter.naydenov/fsm' );
+
+
+
+
+class FsmHub {
+
+constructor ( machine, transitionLib ) {
+        const hub = this;
+
+        hub.fsm = {}
+        const result = this._setTransitions ( machine, transitionLib )
+        hub.transition  = result.transitions
+        hub.subscribers = result.subscribers
+        hub.actions     = result.actions
+    } // constructor func.
+
+
+
+
+
+_setTransitions ( {table, transformers}, transitionLib ) {
+    let 
+          transitions = {}
+        , subscribers = {}
+        , actions    = {}
+        ;
+
+    table.forEach ( (el,i) => {
+            if ( !(el instanceof Array) &&   el.length != 4  ) {
+                    console.log ( `Error: Wrong description record on row ${i+1}.`)
+                    return
+               }
+            const key = `${el[0]}/${el[1]}`;     // fsmName/state
+            subscribers [key] = el[2]
+            actions [`${key}/${el[2]}`] = el[3]  // fsmName/state/fsmSubscriberName
+       })
+    if ( transitionLib )   transitions = Object.assign ( {}, transitionLib )
+
+    return { transitions, subscribers, actions }
+} // _setTransitions func.
+
+
+
+
+
+addFsm ( name, fsm ) {
+        const hub = this;
+        if ( hub[name] ) {
+                console.log ( `FSM "${name}" is already registered` )
+                return
+        }
+        hub [ name ] = fsm
+        fsm.on ( 'update', (state, response) =>  hub._callback (name, state, response)   )
+    } // addFsm func.
+
+
+
+
+
+_callback ( source, state, response ) {
+    const 
+          hub = this
+        , subscribers = hub.subscribers
+        , act = hub.actions
+        , item = `${source}/${state}`
+        , fsmSubscriber = subscribers[item]
+        ;
+    let data;
+    if ( fsmSubscriber ) {
+            // TODO: check for transformations
+            data = response
+            if ( !fsm [ fsmSubscriber ] )   {
+                    console.log ( `Warning: Fsm "${fsmSubscriber}" is not registered to the hub.` )
+                    return
+               }
+            action = act [ `${item}/${fsmSubscriber}`]
+            fsm [ fsmSubscriber ].update ( action, data )
+       } // if fsmSubscriber
+} // _callback func.
+
+} // FsmHub class
---
+++ test/hub.js
@@ -0,0 +1,89 @@
+
+const
+Fsm    = require ('../src/fsm') 
+, chai   = require ( 'chai' )
+, expect = require ( 'expect.js')
+;
+
+describe ( 'Finite State Machine', () => {
+
+
+
+it ( 'Check FSM structure', () => {
+   // SETUP - provide machine description and transition library.
+    const 
+           transformerLib = {
+                        "one2two" : function ( state, resultResponseData ) {
+                                // convert values from miniOne to miniTwo format
+                                return resultResponseData
+                            }
+                    }
+        ,  machine = {
+                       table : [
+                                   // [ fsm, onStateChange, listenerFsm , action  ]
+                                      [ 'miniOne', 'active', 'miniTwo', 'activate'  ]
+                                    , [ 'miniOne', 'off',    'miniTwo', 'switchOFF' ]
+                                    , [ 'miniTwo', 'off',    'miniOne', 'switchOFF' ]
+                                ]
+                      , transformers : {
+                                      // "from/to" : functionName
+                                     'miniOne/miniTwo' : 'one2two' 
+                                  }       
+                 }
+        , miniOne = {
+                          init : 'none'
+                        , table : [
+                                    [ 'none', '']
+                                ]
+                }
+        , miniTwo = {
+                          init : 'none'
+                        , table : [
+
+                                ]
+                }
+        ;
+
+   
+
+
+
+    // HOW IT SHOULD WORK?
+
+    /**
+     *    
+     *  *** FSM orchestration and state managment tool
+     *   - Register fsm
+     *   - Create and register fsm
+     *   - Register for fsm changes
+     *   - Library with listener functions
+     *   - listener function arguments ( state, responseData )
+     *   - listener function response - void
+     *   - Internal callback function
+     *       Started on "update" event with params from the table
+     *   - description table:
+     *     [ source, onStateChange, listenerFsm , action  ]
+     *     fsm - listened fsm
+     *     onStateChange - read state after "update" event
+     *     listenerFsm  - Other fsm that should react on change
+     *     action - update reaction from listenerFsm
+     *     transformerLib - result.response in different FSM could vary. 
+     *        TransformerLib functions will take care to make data compatible 
+     *        across the hub. TransformerLib should be optional. Only if you need
+     *        it.
+     *     transformerFunction - apply to result.response data before apply 
+     *     the listenerFSM "update"
+     * 
+     */
+
+    let hub = new FsmHub ( machine, transformerLib );
+
+    
+   
+}) // it minimal working configuration
+
+
+
+}) // describe
+
+
