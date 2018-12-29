e0125269fbddcf0bab9bf4c6d8b1ee3cd025497d
Peter Naydenov <free@backformation.com>
2018-12-29 05:13:36 +0200

    Documentation update

--- README.md
+++ README.md
@@ -121,8 +121,82 @@ Transformer function are receiving two arguments - **fsm state after update** an
 
 
 ## Example
+Create hub and register two fsm machines: **oneFsm** and **twoFsm**. Register also a callback function **showme**.
+
+Hub should start **twoFsm** when **oneFsm** is started. When **twoFsm** is `active` trigger a **showme** callback function. The transformer `twoFsm/showme` should be applied.
 
 ```js
+// Define Fsm machines
+    const 
+         miniOne = {
+                          init : 'none'
+                        , table : [
+                                    [ 'none', 'activate', 'active', 'switchOn']
+                                ]
+                };
 
+// Setup fsm transition libraries
+    const transitionOne = {
+                    switchOn ( task, dependencies, stateObj, dt ) {
+                                task.done ({ 
+                                          success  : true 
+                                        , response : dt
+                                    })
+                        }
+            };
+
+
+
+// Init fsm machines
+    const  // oneFsm and twoFsm are identical fsm machines
+          oneFsm = new Fsm ( miniOne, transitionOne  )
+        , twoFsm = new Fsm ( miniOne, transitionOne  )
+        ;
+
+
+
+
+// Define hub
+const 
+        machine = {
+                        reactivity : [
+                                    //    [ fsm, onStateChange, listenerFsm , action  ]
+                                          [ 'oneFsm', 'active', 'twoFsm', 'activate'  ]
+                                    //    [ 'fsm',     'state', 'callbackFn' ]
+                                        , [ 'twoFsm', 'active', 'showme' ]
+                                    ]
+                        , transformers : {
+                                        // "from/to" : functionName
+                                        'twoFsm/showme' : 'simple'
+                                    }       
+                    }
+      , transformerLib = {
+                        simple : function ( state, resultResponseData ) {
+                                        return {
+                                                  second : 'second'
+                                                , state
+                                                , 'answer' : resultResponseData
+                                            }
+                                    } 
+                    }
+      ;
+
+// Initialize the hub
+    let hub = new FsmHub ( machine, transformerLib );
+
+    function showme (transitionResult) {
+                console.log ( 'ShowMe' )
+                console.log (transitionResult)
+        } // showme func.
+
+    hub.addFsm ({ oneFsm, twoFsm })
+    hub.addFunctions ( { showme })
+
+// Start!
+    oneFsm.update ( 'activate', 'try' )
+
+// Result on the screen:
+// ShowMe
+// { second: 'second', state: 'active', answer: 'try' }
 
 ```
--- test/hub.js
+++ test/hub.js
@@ -10,94 +10,74 @@ describe ( 'Finite State Machine', () => {
 
 
 it ( 'Check FSM structure', () => {
-   // SETUP - provide machine description and transition library.
+// Define Fsm machines
     const 
-           transformerLib = {
-                        "one2two" : function ( state, resultResponseData ) {
-                                // convert values from miniOne to miniTwo format
-                                return { 
-                                          state
-                                        , 'answer' : resultResponseData 
-                                    }
-                            }
-                        , simple : function ( state, resultResponseData ) {
-                                console.log ( 'hello from second transformation' )
-                                return {
-                                            second : 'second'
-                                          , state
-                                          , 'answer' : resultResponseData
-                                        }
-                            } 
-                    }
-        ,  machine = {
-                       reactivity : [
-                                   // [ fsm, onStateChange, listenerFsm , action  ]
-                                      [ 'oneFsm', 'active', 'twoFsm', 'activate'  ]
-                                    , [ 'miniOne', 'off',    'miniTwo', 'switchOFF' ]
-                                    , [ 'miniTwo', 'off',    'miniOne', 'switchOFF' ]
-                                    //[ 'fsm',     'state', 'callbackFn' ]
-                                    , [ 'oneFsm', 'active', 'yo'        ]
-                                    , [ 'twoFsm', 'active', 'showme']
-                                ]
-                      , transformers : {
-                                      // "from/to" : functionName
-                                       'oneFsm/twoFsm' : 'one2two'
-                                     , 'twoFsm/showme' : 'simple' 
-                                  }       
-                 }
-        , miniOne = {
+         miniOne = {
                           init : 'none'
                         , table : [
                                     [ 'none', 'activate', 'active', 'switchOn']
                                 ]
-                }
-        , miniTwo = {
-                          init : 'none'
-                        , table : [
-                                    ['none', 'activate', 'active', 'switchOn' ]
-                                ]
-                }
+                };
+
+// Setup fsm transition libraries
+    const transitionOne = {
+                    switchOn ( task, dependencies, stateObj, dt ) {
+                                task.done ({ 
+                                          success  : true 
+                                        , response : dt
+                                    })
+                        }
+            };
+
+
+
+// Init fsm machines
+    const  // oneFsm and twoFsm are identical
+          oneFsm = new Fsm ( miniOne, transitionOne  )
+        , twoFsm = new Fsm ( miniOne, transitionOne  )
         ;
 
-    function switchOn ( task, dependencies, stateObj, dt ) {
-            task.done ({ 
-                      success  : true 
-                    , response : dt
-                })
-        }
 
-    function switchOnSecond ( task, dependencies, stateObj, dt ) {
-            task.done ({ 
-                      success  : true 
-                    , response : dt
-                })
-        }
 
-    const 
-          oneFsm = new Fsm ( miniOne, { switchOn}  )
-        , twoFsm = new Fsm ( miniTwo, { switchOn: switchOnSecond}  )
-        ;
 
+// Define hub
+const 
+        machine = {
+                        reactivity : [
+                                    //    [ fsm, onStateChange, listenerFsm , action  ]
+                                          [ 'oneFsm', 'active', 'twoFsm', 'activate'  ]
+                                    //    [ 'fsm',     'state', 'callbackFn' ]
+                                        , [ 'twoFsm', 'active', 'showme' ]
+                                    ]
+                        , transformers : {
+                                        // "from/to" : functionName
+                                        'twoFsm/showme' : 'simple'
+                                    }       
+                    }
+      , transformerLib = {
+                        simple : function ( state, resultResponseData ) {
+                                        return {
+                                                  second : 'second'
+                                                , state
+                                                , 'answer' : resultResponseData
+                                            }
+                                    } 
+                    }
+      ;
+
+// Initialize the hub
     let hub = new FsmHub ( machine, transformerLib );
 
+    function showme (transitionResult) {
+        console.log ( 'ShowMe' )
+        console.log (transitionResult)
+    } // showme func.
+
     hub.addFsm ({ oneFsm, twoFsm })
+    hub.addFunctions ( { showme  })
 
-    hub.addFunctions ( {
-          'yo'     : transitionResult => {
-                            console.log ( 'YO' )
-                            console.log (transitionResult)
-                        }
-        , 'showme' : transitionResult => {
-                            console.log ( 'ShowMe' )
-                            console.log (transitionResult)
-                        }
-    })
-
-    // console.log ( hub )
+// Start!
     oneFsm.update ( 'activate', 'try' )
-
-       
-
 }) // it minimal working configuration
 
 
